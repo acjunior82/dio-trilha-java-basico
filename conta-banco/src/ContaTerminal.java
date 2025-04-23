487500d893bf1e16596ab6aa9c049ed5b4b80906
acjunior82 <acjunior82@gmail.com>
2025-04-23 20:54:32 -0300

    conta-banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Por Favor digite o número de sua AGENCIA");
+        int agencia = scanner.nextInt();
+
+
+        System.out.println("Por Favor digite o número de sua CONTA");
+        int conta = scanner.nextInt();
+
+        System.out.println("Por Favor digite o seu NOME");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por Favor digite o seu SALDO");
+        double saldo =scanner.nextDouble();
+        
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + 
+        " conta " + conta + " e seu saldo de R$ " + saldo + " já está disponível para saque");
     }
 }
